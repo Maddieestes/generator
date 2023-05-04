238806bcf5be1d25cc7daa7450ffd128217a0981
Madeline Estes <maddiegirl.360@gmail.com>
2023-05-03 17:45:17 -0700

    commit

--- Assets/script.js
+++ Assets/script.js
@@ -5,11 +5,30 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  // testing to make sure button is functional
+  console.log ("button pressed")
+
+
+
+
+
+
+
+
+// The password that will apper after the button is pressed
+  return("Password has been created")
+
+}
 // Write password to the #password input
+
+// This generates the password
 function writePassword() {
   var password = generatePassword();
+  // Make generate password function
   var passwordText = document.querySelector("#password");
 
+  // This displays password on screen
   passwordText.value = password;
 
 }
