956bac0f0ed7653942d51bd7b390e8ddfb4e9b22
Madeline Estes <maddiegirl.360@gmail.com>
2023-05-04 20:22:20 -0700

    commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,37 +1,82 @@
-
-// Assignment code here
-
-
-// Get references to the #generate element
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
-  // testing to make sure button is functional
-  console.log ("button pressed")
-
-
-
-
-
-
-
-
-// The password that will apper after the button is pressed
-  return("Password has been created")
-
-}
 // Write password to the #password input
-
-// This generates the password
 function writePassword() {
   var password = generatePassword();
-  // Make generate password function
   var passwordText = document.querySelector("#password");
-
-  // This displays password on screen
-  passwordText.value = password;
-
+  if (password) {
+    passwordText.value = password;
+  } else {
+    passwordText.value = '';
+  }
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Arrays to make statements easier
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var symbol = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "<", "=", ">", "?", "@", "~"];
+
+
+function generatePassword() {
+  var text = "";
+  var newArray = [];
+  let Length = prompt("How long would you like your password to be? Choose a number between 8-128.");
+
+  // Password requirements
+  if (isNaN(Length)) {
+    alert("Please Try again! You have to enter a number.");
+    return;
+  } else if (Length < 8 || Length > 128) {
+    alert(" Please try again! Number must be between 8-12.");
+    return;
+  };
+
+  // prompting users input and confirming info
+
+  let pickUp = "Do you want to have upper case letters in your password?";
+  choiceUp = confirm(pickUp);
+
+  let pickLower = "Do you want to have lower case letters in your password?";
+  choiceLower = confirm(pickLower);
+
+  let pickNumber = "Do you want to add numbers to your password?";
+  choiceNumber = confirm(pickNumber);
+
+  let pickSymbol = "Do you want to add symbols to your password?";
+  choiceSymbol = confirm(pickSymbol);
+
+  // Character sets to choose from
+  if (!choiceUp && !choiceLower && !choiceNumber && !choiceSymbol) {
+    alert("Please pick at least one character option. ");
+    var newArray = [];
+    return;
+  } else {
+    // added if statements to have the generator know what to do with the user inputs
+    if (choiceUp == true) {
+      newArray = newArray.concat(upper);
+    }
+
+    if (choiceLower == true) {
+      newArray = newArray.concat(lower);
+    }
+
+    if (choiceNumber == true) {
+      newArray = newArray.concat(number);
+    }
+
+    if (choiceSymbol == true) {
+      newArray = newArray.concat(symbol);
+    }
+
+  }
+
+  //Building the password
+  for (var i = 0; i < Length; i++)
+    text += newArray[Math.floor(Math.random() * newArray.length)];
+  return text;
+}
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # generator
+Description:
+I created this site to practice my Javascript skills. I learned throughout this project that I have a lot to learn regarding Javascript. I faced some challenges while working such as getting the password to generate, figuring out how to set the min and max for the length of the password, and lastly understanding functions. I overcame these challenges by researching documentation and asking my classmates for assistance on the parts I was struggling with. However while facing these challenges I learned more about arrays, math.random, and math.floor which I plan to take that knowledge with me on future projects.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Credit:
+Starter Code for this project is https://github.com/coding-boot-camp/friendly-parakeet I did not create the starter code.
+Referenced Javascript mini project that was done in class.
+https://www.w3schools.com/jsref/jsref_isinteger.asp 
+https://www.w3schools.com/jsref/met_win_alert.asp 
+https://www.w3schools.com/js/js_arrays.asp
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+https://owasp.org/www-community/password-special-characters 
+https://stackoverflow.com/questions/42678891/typescript-character-type
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else 
+https://stackoverflow.com/questions/44591392/if-else-statement-with-prompt
+https://www.w3schools.com/jsref/jsref_obj_string.asp 
+https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm
